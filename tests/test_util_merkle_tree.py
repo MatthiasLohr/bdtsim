154fb03e8ca8f9311bd59e6a9463d79e6d0a74be
Matthias Lohr <mail@mlohr.com>
2020-05-06 21:16:01 +0200

    added support for merkle tree generation

--- tests/test_util_merkle_tree.py
+++ tests/test_util_merkle_tree.py
@@ -15,9 +15,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import merkle_tree
+import unittest
+
+from bdtsim.data_provider import RandomDataProvider
+from bdtsim.util import merkle_tree
 
 
-__all__ = [
-    'merkle_tree'
-]
+class UtilMerkleTreeTest(unittest.TestCase):
+    def test_from_file(self):
+        data_provider = RandomDataProvider(size=15, seed=42)
+        merkle_tree_root = merkle_tree.from_file(data_provider.file_pointer, 4)
+        last_leaf = merkle_tree_root.children[1].children[1]
+        self.assertIsInstance(last_leaf, merkle_tree.MerkleTreeLeaf)
+        self.assertEqual(3, len(last_leaf.data))
+        self.assertEqual('67b7786d13996b4e46375ceb603ef57707487107', merkle_tree_root.digest('sha1').hex())
