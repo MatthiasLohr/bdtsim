7d3700f0b6cbd4dcf8f4a60749a3f1dfffdf085f
Jonathan Hund <Jonathan.Hund@voestalpine.com>
2020-05-28 16:45:56 +0200

    TODO: overflow problem

--- bdtsim/protocol/delgado/delgado-trade/ecdsa-vuln.sol
+++ bdtsim/protocol/delgado/delgado-trade/ecdsa-vuln.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.6.0;
 
 contract Delgado {
+    /**
+   * @dev Recover signer address from a message by using their signature
+   * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+   * @param sig bytes signature, the signature is generated using web3.eth.sign()
+   */
+   uint _z1;
+   uint _s1;
+   uint _r;
+   
     function recover(bytes32 hash, bytes memory sig)
         internal
         pure
@@ -46,26 +55,38 @@ contract Delgado {
         address addr =  0x8e6a1F13a9C6b9443fEa4393291308AC4c965B69;
         return recover(message, sig) == addr;
     }
-    function testVulnerability() public pure returns (bool){
+    function testVulnerability() public pure returns (bool,uint){
         uint r  = 0xd47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad1;
         uint s1 = 0x78c9d47ef31caf0102f9ae2489d7c78ab51692ddd898b6eb20b16a0d25b01c78;
         uint z1 = 0x4435b0704795962ac9efe71b841a5366434f552d8b5beca04a48426c15fd9ad7;
         uint s2 = 0x240bcda3967d66c71c92ffc4c4486d99968183f198c5fe1612a5cc99a05ba99a;
         uint z2 = 0x6b8bb3201a7ce4c7ed72eddc46d9b6d7350bc2eb8c28df9763518de8d66b0b52;
-        return retrievePrivateKey(r,s1,s2,z1,z2);
+        return verifySellerPrivateKey(r,s1,s2,z1,z2);
     }
     
-    function retrievePrivateKey(uint r,uint s1, uint s2, uint z1, uint z2) private pure returns (bool){
+    function verifySellerPrivateKey(uint r,uint s1, uint s2, uint z1, uint z2) private pure returns (bool,uint){
         //order of base point G of secp256k1
         uint n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141;
         
         //k of two signatures with the same key generated with the same k
-        uint k = (z1 - z2) * expmod(s1 - s2,n-2,n) % n;
+        uint k = (z1 - z2) * longpow(s1 - s2,n-2,n) % n;
         
-        uint x1 = (s1*k-z1) *expmod(r,n-2,n) % n;
-        uint x2 = (s1*k-z1) *expmod(r,n-2,n) % n;
-        return x1 == x2;
+        uint x1 = (s1*k-z1) *longpow(r,n-2,n) % n;
+        uint x2 = (s2*k-z2) *longpow(r,n-2,n) % n;
+        return (x1 == x2,k);
 
+    }
+    function longpow(uint x, uint y, uint z) internal pure returns (uint r) {
+    uint number = 1;
+    while (y!= 0)
+    {
+        if ((y & 1)==0){
+            number = number * x % z;
+        }
+        y >>= 1;
+        x = x * x % z;
+    }
+    return number;
     }
     //cheap modular multiplicative inverse
     function expmod(uint b, uint e, uint m) internal pure returns (uint r) {
@@ -80,9 +101,9 @@ contract Delgado {
         bit = bit;
         assembly {
            for { } true { } {
-                if eq(bit, 0){
-                    break
-                }
+               if iszero(bit){
+                   break
+               }
                 r := mulmod(mulmod(r, r, m), exp(b, iszero(iszero(and(e, bit)))), m)
                 r := mulmod(mulmod(r, r, m), exp(b, iszero(iszero(and(e, div(bit, 2))))), m)
                 r := mulmod(mulmod(r, r, m), exp(b, iszero(iszero(and(e, div(bit, 4))))), m)
@@ -91,13 +112,12 @@ contract Delgado {
             }
         }
     }
-      /*function retrievePrivateKey(uint k, uint s1, uint s2, uint z1, uint z2) private pure returns (bool){
-        uint sdelta = s1-s2;
-        uint mdelta = z1-z2;
-        uint secret = sdelta / mdelta;
-        uint x1 = (s1*secret-z1)/k;
-        uint x2 = (s2*secret-z2)/k;
-        return x1 == x2;
-      }
-    */
+    
+    function BuyerRequestKey(uint z1, uint s1, uint r ) public payable{
+        _z1 = z1;
+        _s1 = s1;
+        _r = r;
+    }
+    function SellerRetrieveEther(uint z2,uint s2) public {
+    }
 }
