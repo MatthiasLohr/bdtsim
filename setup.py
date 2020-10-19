3e1d8d3ae75144c44155d5c754c9dbc1bd0ed260
Matthias Lohr <mail@mlohr.com>
2020-10-19 14:02:34 +0200

    preparations to support python 3.9 (#38)

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering',
     ],
     project_urls={
